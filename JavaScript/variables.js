0e3dfab897ab7f3ea74651b334609be20db1bbad
Bhut Milap <bhutmilap@gmail.com>
2026-08-17 13:05:13 +0530

    m

---
+++ JavaScript/variables.js
@@ -0,0 +1,73 @@
+// /*function Morning() {
+//     console.log("Good Morning");
+// }
+// function Afternoon() {
+//     console.log("Good Afternoon");
+// }
+// function Evening() {
+//     console.log("Good Evening");
+// }
+
+// let time = "Morning";
+
+// switch (time) {
+//     case "Morning":
+//         Morning();
+//         break;
+//     case "Afternoon":
+//         Afternoon();
+//         break;
+//     case "Evening":
+//         Evening();
+//         break;
+// }*/
+
+
+// /* let age = 19;
+// let licenese;
+// if(age >= 18){
+//     licenese = true;
+// }
+// else{
+//     licenese = false;
+// }
+// check(licenese);
+// function check(licenese){
+//     if(licenese){
+//         console.log("You can drive");
+//     }
+//     else{
+//         console.log("You cannot drive");
+//     }
+// }*/
+
+// let marks = 60;
+// if (marks >= 90) {
+//     console.log("A+");
+// }
+// else if (marks >= 80) {
+//     console.log("A");
+// }
+// else if (marks >= 70) {
+//     console.log("B");
+// }
+// else if (marks >= 60) {
+//     console.log("C");
+// }
+// else if (marks >= 33) {
+//     console.log("D");
+// }
+// else if (marks <= 32) {
+//     console.log("Fail");
+// }
+// else {
+//     console.log("Invalid marks");
+// }
+
+// function demo() {
+//     console.log("Hello World");
+// }
+let b = function() {
+    console.log("Hello World");
+}
+b();
